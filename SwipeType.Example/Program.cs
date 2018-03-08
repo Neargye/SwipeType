9dd56462c9632f4dd862d95a66e0d906e3abc695
Terik23 <neargye@gmail.com>
2018-03-08 19:57:02 +0500

    Add Example

---
+++ SwipeType.Example/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace SwipeType.Example
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var swype = new SimpleSwipeType(File.ReadAllLines("EnglishDictionary.txt"));
+            string[] testCases =
+            {
+                "heqerqllo",
+                "qwertyuihgfcvbnjk",
+                "wertyuioiuytrtghjklkjhgfd",
+                "dfghjioijhgvcftyuioiuytr",
+                "aserfcvghjiuytedcftyuytre",
+                "asdfgrtyuijhvcvghuiklkjuytyuytre",
+                "mjuytfdsdftyuiuhgvc",
+                "vghjioiuhgvcxsasdvbhuiklkjhgfdsaserty"
+            };
+
+            foreach (var s in testCases)
+            {
+                Console.WriteLine("#===============================#");
+                Console.WriteLine($"Raw string: {s}");
+                var result = swype.GetSuggestion(s);
+                for (var i = 0; i < result.Length; i++)
+                {
+                    var x = result[i];
+                    Console.WriteLine("match " + (i + 1) + ": " + x);
+                }
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
