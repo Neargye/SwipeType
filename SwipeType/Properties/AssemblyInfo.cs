17f66e7ec5d51a69209bb769b5b980a8e4541b16
neargye <neargye@gmail.com>
2019-05-20 21:46:06 +0500

    v0.1.1

--- SwipeType/Properties/AssemblyInfo.cs
+++ SwipeType/Properties/AssemblyInfo.cs
@@ -10,11 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Daniil Goncharov 2018 - 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
-
 [assembly: Guid("9277bede-c918-4b17-8a4a-32580c0f547a")]
-
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyVersion("0.1.1")]
+[assembly: AssemblyFileVersion("0.1.1")]
 [assembly: NeutralResourcesLanguage("en")]
